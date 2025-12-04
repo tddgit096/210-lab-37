cc92a54d8e41277647d3e8910b3d5908855ee839
tddgit096-pc <>
2025-12-03 18:52:08 -0800

    Milestone 3

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Lab 37 | Toma Dimov | COMSC210
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -8,13 +9,12 @@ string INPUTFILE = "data.txt";
 int HASHSIZE = 70 , TESTRUN = 100;
 
 int gen_hash_index(string);
-int read_file(string, map<int,list<string>>);
+int read_file(string, map<int,list<string>>&);
 void print(map<int,list<string>>, int);
 
 int main() {
     map <int,list<string>> hash_table; // a map of int-keys storing string-lists
     read_file(INPUTFILE,hash_table);
-    cout<<"TEST "<<hash_table[8].front()<<endl;
     print(hash_table,TESTRUN);
     return 0;
 }
@@ -29,14 +29,13 @@ int gen_hash_index(string s){
 }
 
 //function reads input file and sums up the entire file.
-int read_file(string inputfile, map<int,list<string>> table){
+int read_file(string inputfile, map<int,list<string>> &table){
     int sum = 0;
     fstream file(inputfile);
     if(file.is_open()){
         string line;
         while(getline(file,line)){
             pair<int, string> hash_pair = {gen_hash_index(line), line}; //store hash and string into pair
-            cout<<"inputting: key "<<hash_pair.first<<" with val"<<hash_pair.second<<endl;
             table[hash_pair.first].push_back(hash_pair.second);       //access map by hash key. insert the string into that key's list.
         }
         file.close();
@@ -52,11 +51,11 @@ int read_file(string inputfile, map<int,list<string>> table){
 void print(map<int,list<string>> table, int limit =-1){
     if(limit == -1) //defaults to unlimited size
         limit = table.size();
-    cout<<"Displaying the first "<<TESTRUN<<" entries to the console:\n";
+    cout<<"Displaying the first "<<limit<<" entries to the console:\n";
     int count = 0; //counter for limiting the iteration
     for(auto pair : table){
         for(string s: pair.second){
-            if(count>=TESTRUN)
+            if(count>=limit)
                 return;
             cout<<s<<endl;
             count++;
@@ -70,16 +69,4 @@ These targets are present in the dataset and can be used for testing:
 1DA9D64D02A0
 666D109AA22E
 E1D2665B21EA
-*/
-
-
-/*
-    //test code for ascii summing
-    string test = "Hello World";
-    cout<<test <<" --> ascii sum:  "<<sum_ascii(test)<<endl;
-
-    cout<<endl<<"Manual Confirmation: ";
-    cout<<((int)'H' +(int)'e' +(int)'l' +(int)'l' +(int)'o' +(int)' ' +(int)'W' +(int)'o' +(int)'r' +(int)'l' +(int)'d'); 
-    cout<<endl;
-
 */
